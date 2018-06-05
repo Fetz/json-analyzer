7eea179101c1ef0d5329b203aea016bd1e0baef2
Luis Silva <fetzpt@gmail.com>
2018-06-06 00:44:12 +0100

    group common packages in a single PR

--- config.js
+++ config.js
@@ -2,7 +2,21 @@ module.exports = {
   labels: ['upgrade', 'renovate'],
   repositories: [
     {
-      repository: 'Fetz/json-analyzer'
+      repository: 'Fetz/json-analyzer',
+      packageRules: [
+        {
+          packagePatterns: ['@commitlint/'],
+          groupName: 'eslint packages'
+        },
+        {
+          packagePatterns: ['^eslint'],
+          groupName: 'eslint packages'
+        },
+        {
+          packagePatterns: ['^rollup'],
+          groupName: 'rollup packages'
+        }
+      ]
     }
   ]
 };
